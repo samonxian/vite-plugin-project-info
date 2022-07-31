6a51dc0bc606c60a8a986588f0fe6ebaad132cc8
samonxian <samonxian@tencent.com>
2022-07-31 12:15:33 +0800

    test: 💍 更新单元测试

--- src/__tests__/createCode.spec.ts
+++ src/__tests__/createCode.spec.ts
@@ -3,10 +3,10 @@ import fs from 'fs-extra';
 import babel from '@babel/core';
 import path from 'path';
 import pkg from '../../package.json';
-import createCode from '../createCode';
+import { createCode, DEFAULT_LOCALE, EN_LOCALE } from '../createCode';
 
 describe('createCode', () => {
-  it('create code correctly', () => {
+  it('create default zh_cn code correctly', () => {
     const code = createCode();
     expect(code).toContain(`repository: '${pkg.repository.url}'`);
     expect(code).toContain(`version: '${pkg.version}'`);
@@ -27,5 +27,22 @@ describe('createCode', () => {
     expect(name).toEqual(pkg.name);
     expect(author).toEqual(pkg.author);
     expect(description).toEqual('');
+
+    expect(code).toContain(DEFAULT_LOCALE.buildTime);
+    expect(code).toContain(DEFAULT_LOCALE.projectAuthor);
+    expect(code).toContain(DEFAULT_LOCALE.projectDescription);
+    expect(code).toContain(DEFAULT_LOCALE.projectName);
+    expect(code).toContain(DEFAULT_LOCALE.projectVersion);
+    expect(code).toContain(DEFAULT_LOCALE.repositoryLink);
+  });
+
+  it('create english code correctly', () => {
+    const enCode = createCode({ locale: EN_LOCALE });
+    expect(enCode).toContain(EN_LOCALE.buildTime);
+    expect(enCode).toContain(EN_LOCALE.projectAuthor);
+    expect(enCode).toContain(EN_LOCALE.projectDescription);
+    expect(enCode).toContain(EN_LOCALE.projectName);
+    expect(enCode).toContain(EN_LOCALE.projectVersion);
+    expect(enCode).toContain(EN_LOCALE.repositoryLink);
   });
 });
